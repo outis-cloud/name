0617c7f3733ef2fb0ffdeaffe02fdb18e81949f7
OuTiS92 <amirhoseinalahdadi76@gmail.com>
2024-04-27 02:03:06 +0330

    updated

--- Install.sh
+++ Install.sh
@@ -92,7 +92,7 @@ finish() {
 	 echo -e " \n"
 	 read -p " Please Enter Your Ip server:" $ip
 	 
-	 if [ -n  $domain ] && [ -n $ip ]; then
+	 if [ -n  "$domain" ] && [ -n "$ip" ]; then
                filedb
                changenameserver
                finish
