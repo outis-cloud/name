a6f5a3d373ebe7034ecba58fc1751aa0154d35c1
OuTiS92 <amirhoseinalahdadi76@gmail.com>
2024-04-27 01:36:56 +0330

    updated

--- Install.sh
+++ Install.sh
@@ -20,7 +20,54 @@ rest='\033[0m'
 plain='\033[0m'
 
 
+#check root 
 
+[[ $EUID -ne 0 ]] && echo "${red} Error Error :${plain} Please run this script with root \n " && exit 1
+
+
+filedb() {
+	        cd /etc/bind/
+        	touch $domain.db
+		cat <<EOL > $domain.db
+$TTL 3600
+@       IN      SOA     ns1.$domain.      hostmaster.$domain. (
+                                                2024042710
+                                                3600
+                                                3600
+                                                1209600
+                                                86400 )
+
+$domain.      3600    IN      NS      ns1.$domain.
+$domain.      3600    IN      NS      ns2.$domain.
+
+$domain.      3600    IN      A       $ip
+ftp     3600    IN      A       $ip
+mail    3600    IN      A       $ip
+ns1     3600    IN      A       $ip
+ns2     3600    IN      A       $ip
+pop     3600    IN      A       $ip
+smtp    3600    IN      A       $ip
+www     3600    IN      A       $ip 
+
+$domain.      3600    IN      MX      10 mail.$domain.
+
+
+
+_acme-challenge 5       IN      TXT     "YRgRCOcKNeGDuBeTEcD4biN4uj_e0CDUIGEitzl519U"
+$domain.      3600    IN      TXT     "v=spf1 a mx ip4:$ip ~all"
+
+EOL
+}
+
+
+
+
+changenameserver() {
+       cd /etc/bind/
+       touch $domain.db
+       echo -e "zone "$domain" { type master; file "/etc/bind/$domain.db"; };" >> named.conf
+
+}
 
 
 
@@ -36,29 +83,22 @@ finish() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-finish
-sleep 2 
-exit 1
-
-#check root 
-
-[[ $EUID -ne 0 ]] && echo "${red} Error Error :${plain} Please run this script with root \n " && exit 1
+ nameserver() {
+	 read -p "${yellow} Please Enter Domains Name:" $domain        
+	 read -p "${yellow} Please Enter Your Ip server:" $ip
+	 
+	 if [ -z  "$domain" ] && [ -z "$ip" ]; then
+        	echo "Please A domain Real And Ip server "
+         else
+               filedb
+               changenameserver
+               finish
+	 fi
+ }
+														       
 
 
 # check OS 
-
-
-
 
 
 if [[ -f /etc/os-release ]]; then 
@@ -87,9 +127,9 @@ centos() {
 
 
 if [[ "${release}" == "centos" ]]; then
-         echo "vvvvvvvvvv"
+       		 centos
 elif [[ "${release}" == "ubuntu" ]]; then
-           ubuntu
+	        ubuntu
 else
 echo -e " ${red} Failed to check Os. Your Os System not Supported " && exit 1
 fi
@@ -109,77 +149,5 @@ fi
 #		exit 1;;
 #esac
 #}
-
-
-sleep 1
-
-nameserver() { 
-
-	read -p "${yellow} Please Enter Domains Name:" $domain
-	read -p "${yellow} Please Enter Your Ip server:" $ip
-
-	if [ -z  "$domain" ] && [ -z "$ip" ]; then
-		echo "Please A domain Real And Ip server "
-	else
-		filedb
-		changenameserver
-		finish
-	fi
-}
-
-filedb() {
-	cd /etc/bind/
-	touch $domain.db
-	cat <<EOL > $domain.db
-	$TTL 3600
-@       IN      SOA     ns1.$domain.      hostmaster.$domain. (
-                                                2024042710
-                                                3600
-                                                3600
-                                                1209600
-                                                86400 )
-
-$domain.      3600    IN      NS      ns1.$domain.
-$domain.      3600    IN      NS      ns2.$domain.
-
-$domain.      3600    IN      A       $ip
-ftp     3600    IN      A       $ip
-mail    3600    IN      A       $ip
-ns1     3600    IN      A       $ip
-ns2     3600    IN      A       $ip
-pop     3600    IN      A       $ip
-smtp    3600    IN      A       $ip
-www     3600    IN      A      	$ip 
-
-$domain.      3600    IN      MX      10 mail.$domain.
-
-
-
-_acme-challenge 5       IN      TXT     "YRgRCOcKNeGDuBeTEcD4biN4uj_e0CDUIGEitzl519U"
-$domain.      3600    IN      TXT     "v=spf1 a mx ip4:$ip ~all"
-
-EOL
-}
-
-
-changenameserver() {
-	cd /etc/bind/
-	touch $domain.db
-	echo -e "zone "$domain" { type master; file "/etc/bind/$domain.db"; };" >> named.conf
-
-}
-
-
-
-finish() {
-	echo -e " "
-	echo -e " "
-	echo -e " "
-	echo -e " "
-	echo -e "Now Finish Nameservers :\n "
-	echo -e  "ns1.$domain \t $ip  \nns2.$domain \t $ip " |lolcat -as 100
-}
-
-
 
 
